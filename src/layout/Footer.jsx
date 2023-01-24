bf5779f6dd436ef3376e73c81f94558c7e2d9521
Kaarel-Richard Kaarelson <kaarel-richard.kaarelson@ut.ee>
2023-01-24 19:41:59 +0200

    Added footer component

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Row, Col, Container, Nav } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import beerLogo from '../assets/beerLogo.png';
 
 const Footer = () => {
   return (
-    <footer className="bottom-0 mb-0 bg-white w-100 shadow-sm py-4">
+    <footer className="bottom-0 mt-auto bg-white w-100 shadow-sm py-4">
       <Container className="py-3 border-top">
         <Row>
           <Col md xs={6} className="d-flex align-items-center">
             <a
               href="/"
               className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-              <img src={beerLogo} width="50px"></img>
+              <img src={beerLogo} width="50px" />
             </a>
             <span className="text-muted">© 2021 Kaarel-Richard Kaarelson</span>
           </Col>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,15 +5,15 @@ import Footer from './Footer';
 
 const Layout = () => {
   return (
-    <div className="position-relative">
+    <>
       <Header />
       <main
-        className="d-flex flex-column text-center mx-3 mx-md-5"
-        style={{ height: '75vh', marginTop: '5rem', marginBottom: '5rem' }}>
+        className="d-flex flex-column min-vh-100 text-center mx-3 mx-md-5"
+        style={{ marginTop: '5rem', marginBottom: '5rem' }}>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
